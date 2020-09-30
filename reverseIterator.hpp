dd73eae4dca6259d3ae07ce15bb3a120a4779432
Anias <miguellaplanamarin@gmail.com>
2020-09-30 13:45:14 +0200

    ft_containers starting list iterator

--- reverseIterator.hpp
+++ reverseIterator.hpp
@@ -6,7 +6,7 @@
 /*   By: mlaplana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 15:38:45 by mlaplana          #+#    #+#             */
-/*   Updated: 2020/09/29 15:42:49 by mlaplana         ###   ########.fr       */
+/*   Updated: 2020/09/30 13:12:14 by mlaplana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,24 @@
 template<class T>
 class reverseIterator
 {
+public:
+    typedef T value_type;
+    typedef value_type* pointer;
+    typedef value_type& reference;
+    typedef std::ptrdiff_t difference;
+protected:
+    pointer _ptr;
+public:
+    reverseIterator(): _ptr(nullptr) { }
+    reverseIterator(pointer ptr): _ptr(ptr) { }
+    reverseIterator(const reverseIterator &c): _ptr(c._ptr) { }
+    virtual ~reverseIterator() { }
     
+    reverseIterator &operator=(const reverseIterator &c) { _ptr = c._ptr; return *this; }
+    
+    reference operator*() { 
+        
+    }
 };
 
 #endif
